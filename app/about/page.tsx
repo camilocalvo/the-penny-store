5935027beee8f3b2094401ce0e4208dbd7f638b4
camilocalvo <camilocalvoalcaniz@gmail.com>
2023-01-31 16:29:26 -0500

    Took advantage of Next routing to create an about page

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>This store was created by Camilo Calvo-Alcaniz</p>
+    </div>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,22 @@
+import Link from "next/link";
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html>
       <head />
-      <body>{children}</body>
+      <body>
+        <main>
+          <nav>
+            <Link href="/">Home</Link>
+            <Link href="/about">About Us</Link>
+          </nav>
+        </main>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
